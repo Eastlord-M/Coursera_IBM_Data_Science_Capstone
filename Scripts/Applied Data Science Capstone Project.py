3f8e0be3bfafcd2db3bef4ac0bece762a83a16d1
Eastlord-M <alex.cheng@live.com.my>
2021-02-21 17:22:42 +0800

    Created scripts folder for the project.

---
+++ Scripts/Applied Data Science Capstone Project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Coursera IBM Applied Data Science Capstone Project
+
+# In[1]:
+
+
+# Importing required libraries
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
